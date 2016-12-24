8dc8c818dd74022a6393aa46d70070fdb4cc87e9
Patryk Niedźwiedziński <Patryk Niedwiedziski>
2016-12-24 13:39:40 +0100

    Change uppercase

--- tui.py
+++ tui.py
@@ -3,42 +3,47 @@
 
 #--
 i=0
-REPORT_COUNTER=0
-REPORT_DATA=[]
-CATCH=0
-SHOW=1
+report_counter=0
+report_data=[]
+catch=0
+show=1
 #--
 
 
-def REPORT(MODE, FUNCTION):
-     if(MODE==0):
-          REPORT_DATA[REPORT_COUNTER]=FUNCTION
-          REPORT_COUNTER+=1
-     elif(MODE==1):
-          while(i<=REPORT_COUNTER):
-               print(REPORT_DATA[i])
+def report(mode, function):
+     if(mode==0):
+          report_data[report_counter]=function
+          report_counter+=1
+     elif(mode==1):
+          while(i<=report_counter):
+               print(report_data[i])
 
-def TITLE(TEXT, LENGTH, CHAR):
-    return_value="TITLE("+TEXT+", "+str(LENGTH)+", "+CHAR+")"
-    print((int((LENGTH-len(TEXT))/2)*CHAR)+TEXT+(int((LENGTH-len(TEXT))/2)*CHAR))
-    if(LENGTH==(int((LENGTH-len(TEXT))/2)*CHAR)+TEXT+(int((LENGTH-len(TEXT))/2)*CHAR) ):
-        return return_value + ": SUCCES"
+def title(text, length, char):
+    return_value="title("+text+", "+str(length)+", "+char+")"
+    print((int((length-len(text))/2)*char)+text+(int((length-len(text))/2)*char))
+    if(length==(int((length-len(text))/2)*char)+text+(int((length-len(text))/2)*char) ):
+        return return_value + ": succes"
     else:
-        return "Something goes wrong with " + return_value
-    REPORT(CATCH, TITLE)
+        return "something goes wrong with " + return_value
+    fun=title(text,length,char)
+    report(catch,fun)
 
-def MENU(NUMBER, TEXT, LENGTH):
-    return_value="MENU("+NUMBER+", "+TEXT+", "+str(LENGTH)+")"
-    print("|" + str(NUMBER) + "." + TEXT + ( (LENGTH-2-len(TEXT))*" ")+"|")
-    if(LENGTH==len("|"+str(NUMBER)+"."+TEXT+((LENGTH-2-len(TEXT))*" ")+"|")):
-        return return_value + ": SUCCES"
+def menu(number, text, length):
+    return_value="menu("+number+", "+text+", "+str(length)+")"
+    print("|" + str(number) + "." + text + ( (length-2-len(text))*" ")+"|")
+    if(length==len("|"+str(number)+"."+text+((length-2-len(text))*" ")+"|")):
+        return return_value + ": succes"
     else:
-        return "Something goes wrong with " + return_value
+        return "something goes wrong with " + return_value
+    fun=menu(number,text,length)
+    report(catch,fun)
 
-def LINE(CHAR, LENGTH):
-    return_value= "LINE(" + CHAR + ", " + str(LENGTH) + ")"
-    print(CHAR*LENGTH)
-    if(LENGTH==len(CHAR*LENGTH)):
-        return return_value + ": SUCCES"
+def line(char, length):
+    return_value= "line(" + char + ", " + str(length) + ")"
+    print(char*length)
+    if(length==len(char*length)):
+        return return_value + ": succes"
     else:
-        return "Something goes wrong with " + return_value
+        return "something goes wrong with " + return_value
+    fun=line(char,length)
+    report(catch,fun)
