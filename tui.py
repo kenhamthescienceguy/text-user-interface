baa29c2aaa48b33433990e59ba4e07136e13acfb
Patryk Niedźwiedziński <Patryk Niedwiedziski>
2016-12-24 15:14:20 +0100

    Fix report

--- tui.py
+++ tui.py
@@ -11,39 +11,46 @@ show=1
 
 
 def report(mode, function):
+     global report_counter
+     global report_data
+     global i
      if(mode==0):
-          report_data[report_counter]=function
+          report_data.append(function)
           report_counter+=1
      elif(mode==1):
-          while(i<=report_counter):
+          while(i<report_counter):
                print(report_data[i])
+               i+=1
 
+#DRAW TITLE/HEADER
 def title(text, length, char):
-    return_value="title("+text+", "+str(length)+", "+char+")"
+    return_value="title('"+text+"', "+str(length)+", '"+char+"')"
     print((int((length-len(text))/2)*char)+text+(int((length-len(text))/2)*char))
-    if(length==(int((length-len(text))/2)*char)+text+(int((length-len(text))/2)*char) ):
-        return return_value + ": succes"
+    if(length==len((int((length-len(text))/2)*char)+text+(int((length-len(text))/2)*char))):
+        return_value=return_value + ": succes"
+        report(catch,return_value)
     else:
-        return "something goes wrong with " + return_value
-    fun=title(text,length,char)
-    report(catch,fun)
+        return_value="something goes wrong with " + return_value
+        report(catch,return_value)
 
+#DRAW MENU OPTION
 def menu(number, text, length):
-    return_value="menu("+number+", "+text+", "+str(length)+")"
+    return_value="menu("+number+", '"+text+"', "+str(length)+")"
     print("|" + str(number) + "." + text + ( (length-2-len(text))*" ")+"|")
     if(length==len("|"+str(number)+"."+text+((length-2-len(text))*" ")+"|")):
-        return return_value + ": succes"
+        return_value=return_value + ": succes"
+        report(catch,return_value)
     else:
-        return "something goes wrong with " + return_value
-    fun=menu(number,text,length)
-    report(catch,fun)
+        return_value="something goes wrong with " + return_value
+        report(catch,return_value)
 
+#DRAW A LINE OF CHAR
 def line(char, length):
-    return_value= "line(" + char + ", " + str(length) + ")"
+    return_value= "line('" + char + "', " + str(length) + ")"
     print(char*length)
     if(length==len(char*length)):
-        return return_value + ": succes"
+        return_value=return_value + ": succes"
+        report(catch,return_value)
     else:
-        return "something goes wrong with " + return_value
-    fun=line(char,length)
-    report(catch,fun)
+        return_value="something goes wrong with " + return_value
+        report(catch,return_value)
